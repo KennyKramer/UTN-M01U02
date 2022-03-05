315203f8665309f38906a6038332827fb5470965
KennyKramer <agveron1@gmail.com>
2022-03-05 18:54:02 -0300

    Carga de imagenes 1

--- UTN-M04/transportes/frontend/src/components/layout/Header.js
+++ UTN-M04/transportes/frontend/src/components/layout/Header.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <div>Header</div>
+        <header>
+           <div className='holder'>
+            <div className='logo'>
+                <img src='images/logo.png' width='100' alt="Transportes X" />
+                <h1>Transportes X</h1>
+              </div>
+            </div> 
+        </header>
     );
 }
 
