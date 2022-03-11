6bb2ef2c03ca9cd1f3161f90a896b29fbd35350a
KennyKramer <agveron1@gmail.com>
2022-03-11 08:42:55 -0300

    Trabajo practico Modulo 04 primera parte

---
+++ UTN-M04/transportes/frontend/src/components/Nav.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+        <nav>
+            <div className="holder">
+                <ul>
+                    <li><NavLink activeClassName="activo" exact to='/'>Home</NavLink></li>
+                    <li><NavLink activeClassName="activo" exact to='/nosotros'>Nosotros</NavLink></li>
+                    <li><NavLink activeClassName="activo" exact to='/novedades'>Novedades</NavLink></li>
+                    <li><NavLink activeClassName="activo" exact to='/contacto'>Contacto</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+
+    );
+}
+
+export default Nav;
